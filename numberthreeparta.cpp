9caabff942518d9eff162d7f37b8b73e7af836d8
luciusggg <luciusgshalom@gmail.com>
2024-09-04 00:44:49 +0300

    Update numberthreeparta.cpp

--- numberthreeparta.cpp
+++ numberthreeparta.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-    int first, last;
-    
-int sum_from_to(int first, int last);
-
-    cout << "Enter the first number: ";
-    cin >> first;
-    cout << "Enter the last number: ";
-    cin >> last;
-
-    
-    cout << "The sum from " << first << " to " << last << " is " << sum_from_to(first, last) << endl;
-
-    return 0;
-}
 int sum_from_to(int first, int last) {
     int sum = 0;
     for (int i = first; i <= last; i++) {
         sum += i;
     }
     return sum;
+}
+
+int main() {
+    int result = sum_from_to(1, 5);
+    std::cout << "Sum from 1 to 5 is: " << result << std::endl;
+    return 0;
 }
