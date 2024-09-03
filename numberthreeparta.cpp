ea9ec11b131e7d10e00d6b619ad1deb0b8523e69
luciusggg <luciusgshalom@gmail.com>
2024-09-04 00:22:10 +0300

    Add files via upload

---
+++ numberthreeparta.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int first, last;
+    
+int sum_from_to(int first, int last);
+
+    cout << "Enter the first number: ";
+    cin >> first;
+    cout << "Enter the last number: ";
+    cin >> last;
+
+    // Calculate and display the sum
+    cout << "The sum from " << first << " to " << last << " is " << sum_from_to(first, last) << endl;
+
+    return 0;
+}
+int sum_from_to(int first, int last) {
+    int sum = 0;
+    for (int i = first; i <= last; i++) {
+        sum += i;
+    }
+    return sum;
+}
